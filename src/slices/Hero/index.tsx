05e0de1a8f44525a07354e1f1f1147cbed3168e9
Iryna Howarth <irynahowarth@gmail.com>
2024-09-16 13:18:20 +0100

    [feature]: add GSAP animation for Hero slice (top half)

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,10 +1,15 @@
+'use client';
+
 import { Bounded } from "@/components/Bounded";
 import Button from "@/components/Button";
 import { TextSplitter } from "@/components/TextSplitter";
 import { asText, Content } from "@prismicio/client";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
+gsap.registerPlugin(useGSAP);
 /**
  * Props for `Hero`.
  */
@@ -14,6 +19,34 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
+
+  useGSAP(()=>{
+    const intoTl = gsap.timeline();
+
+    intoTl
+      .set('.hero', {opacity:1})
+      .from(".hero-header-word", {
+        scale: 3,
+        opacity: 0,
+        ease: "power4.in",
+        delay: 0.3,
+        stagger: 1
+      })
+      .from('.hero-subheading', {
+        opacity:0,
+        y:30
+      }, "+=.8")
+      .from('.hero-body', {
+        opacity:0,
+        y: 10
+      })
+      .from('.hero-button', {
+        opacity:0,
+        y: 10,
+        duration:.6
+      })
+  })
+
   return (
     <Bounded
       data-slice-type={slice.slice_type}
@@ -26,10 +59,10 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
             <h1 className="hero-header lg:text-[13rem] text-7xl font-black uppercase leading-[.8] text-orange-500 md:text-[9rem]">
              <TextSplitter text={asText(slice.primary.heading)}  
               wordDisplayStyle="block"
-              className="hero-heder-word"
+              className="hero-header-word"
              />
             </h1>
-            <div className="hero-heading mt-12 text-5xl font-semibold text-sky-950 lg:text-6xl">
+            <div className="hero-subheading mt-12 text-5xl font-semibold text-sky-950 lg:text-6xl">
               <PrismicRichText field={slice.primary.subheading} />
             </div>
             <div className="hero-body text-2xl font-normal text-sky-950">
