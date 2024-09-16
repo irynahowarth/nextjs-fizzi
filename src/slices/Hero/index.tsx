e5e558c190caadc61eaddd447eee0e9ad4dcb331
Iryna Howarth <irynahowarth@gmail.com>
2024-09-16 13:36:47 +0100

    [feature]: add GSAP for Hero slice (bottom half)

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -8,8 +8,9 @@ import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(useGSAP,ScrollTrigger);
 /**
  * Props for `Hero`.
  */
@@ -44,8 +45,44 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
         opacity:0,
         y: 10,
         duration:.6
-      })
+      });
+
+    const scrollTl = gsap.timeline({
+      scrollTrigger:{
+        trigger: '.hero',
+        start: 'top top',
+        end: 'bottom bottom',
+        scrub: 1.5, 
+        markers: true
+      }
+    })
+
+    scrollTl
+      .fromTo('body', {
+        backgroundColor: '#FDE047'
+      }, {
+         backgroundColor: '#D9F99D',
+         overwrite: 'auto'
+      },
+      1
+    )
+    .from('.text-side-heading .split-char', {
+      scale: 1.3,
+      y: 40,
+      rotate: -25,
+      opacity: 0,
+      stagger:.1,
+      ease: 'back.out(3)',
+      duration:.5
+    })
+    .from('.text-side-body',{
+      y: 20,
+      opacity:0
+    })
+
   })
+
+ 
 
   return (
     <Bounded
