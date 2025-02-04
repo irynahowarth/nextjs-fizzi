ddba46dd14ffe1bda4a82d7af75c588bf907f9ab
Iryna Howarth <irynahowarth@gmail.com>
2025-02-04 16:35:52 +0000

    [feature]: set up all 5 cans in Three.js before using GSAP

--- src/slices/Hero/Scene.tsx
+++ src/slices/Hero/Scene.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useRef } from "react"
 import FloatingCan from "@/components/FloatingCan"
 import { Environment } from "@react-three/drei"
 import { Group } from "three"
@@ -7,10 +8,29 @@ import { Group } from "three"
 type Props = {}
 
 export default function Scene({}: Props) {
+    const can1Ref = useRef<Group>(null)
+    const can2Ref = useRef<Group>(null)
+    const can3Ref = useRef<Group>(null)
+    const can4Ref = useRef<Group>(null)
+    const can5Ref = useRef<Group>(null)
     
-  return (
-    <group>
-        <FloatingCan />
+    const can1GroupRef = useRef<Group>(null)
+    const can2GroupRef = useRef<Group>(null)
+    
+    const groupRef = useRef<Group>(null)
+    const FLOAT_SPEED = 1.5
+    return (
+    <group ref={groupRef}> 
+         <group ref={can1GroupRef}> 
+            <FloatingCan  ref={can1Ref} flavor="blackCherry" floatSpeed={FLOAT_SPEED} />
+        </group>
+        <group ref={can2GroupRef}> 
+            <FloatingCan  ref={can2Ref} flavor="lemonLime" floatSpeed={FLOAT_SPEED} />
+        </group>
+        <FloatingCan  ref={can3Ref} flavor="grape" floatSpeed={FLOAT_SPEED} />
+        <FloatingCan  ref={can4Ref} flavor="strawberryLemonade" floatSpeed={FLOAT_SPEED} />
+        <FloatingCan  ref={can5Ref} flavor="watermelon" floatSpeed={FLOAT_SPEED} />
+
         <Environment files="/hdr/lobby.hdr" environmentIntensity={1.5}/>
     </group>
   )
