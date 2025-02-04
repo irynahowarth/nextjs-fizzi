5dcc39b047fd20d4b29519e8b18a36b9c5e9dfbb
Iryna Howarth <irynahowarth@gmail.com>
2025-02-04 22:56:59 +0000

    [feature]: add GSAP for cans and set up starting positions before animation

--- src/slices/Hero/Scene.tsx
+++ src/slices/Hero/Scene.tsx
@@ -2,8 +2,11 @@
 
 import { useRef } from "react"
 import FloatingCan from "@/components/FloatingCan"
-import { Environment } from "@react-three/drei"
-import { Group } from "three"
+import { Environment, OrbitControls } from "@react-three/drei"
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP)
 
 type Props = {}
 
@@ -19,6 +22,34 @@ export default function Scene({}: Props) {
     
     const groupRef = useRef<Group>(null)
     const FLOAT_SPEED = 1.5
+    
+    useGSAP(()=>{
+        if(
+            !can1Ref.current ||
+            !can2Ref.current ||
+            !can3Ref.current ||
+            !can4Ref.current ||
+            !can5Ref.current ||
+            !can1GroupRef.current ||
+            !can2GroupRef.current ||
+            !groupRef.current 
+        ) 
+        return;
+
+        //Set can starting location
+        gsap.set(can1Ref.current.position, {x: -1.5})
+        gsap.set(can1Ref.current.rotation, {z: -0.5})
+          
+        gsap.set(can2Ref.current.position, {x: 1.5})
+        gsap.set(can2Ref.current.rotation, {z: 0.5})   
+        
+        gsap.set(can3Ref.current.position, {y:5, z:2})   
+        gsap.set(can4Ref.current.position, {x: 2, y:4, z:2})   
+        gsap.set(can5Ref.current.position, {y: -5})   
+
+    })
+
+
     return (
     <group ref={groupRef}> 
          <group ref={can1GroupRef}> 
@@ -31,7 +62,8 @@ export default function Scene({}: Props) {
         <FloatingCan  ref={can4Ref} flavor="strawberryLemonade" floatSpeed={FLOAT_SPEED} />
         <FloatingCan  ref={can5Ref} flavor="watermelon" floatSpeed={FLOAT_SPEED} />
 
-        <Environment files="/hdr/lobby.hdr" environmentIntensity={1.5}/>
+        {/* <OrbitControls /> */}
+        <Environment files="/hdr/lobby.hdr"∆ environmentIntensity={1.5}/>
     </group>
   )
 }
