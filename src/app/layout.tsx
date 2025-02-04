1363a5eaf06343f61016423681ea7c4904d53b4f
Iryna Howarth <irynahowarth@gmail.com>
2025-02-04 14:25:26 +0000

    [feature]: add Three.js and support pkgs. Add basic shape in ViewCanvas component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
 import './app.css'
 import Header from '@/components/Header';
+import ViewCanvas from '@/components/ViewCanvas';
 
 const alpino = localFont({
   src: "../../public/fonts/Alpino-Variable.woff2",
@@ -22,6 +23,7 @@ export default function RootLayout({
         <Header />
         <main>
           {children}
+          <ViewCanvas />
         </main>
         </body>
       <PrismicPreview repositoryName={repositoryName} />
