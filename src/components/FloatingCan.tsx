c1072eab6709e7812dea74645b3e3b0f3df10ed0
Iryna Howarth <irynahowarth@gmail.com>
2025-02-04 16:13:41 +0000

    [feature]: add SodaCan component and connected files. Make can float with FloatingCan component

---
+++ src/components/FloatingCan.tsx
@@ -0,0 +1,44 @@
+"use client"
+import { SodaCan, SodaCanProps } from "@/components/SodaCan"
+import { Float } from "@react-three/drei"
+import { forwardRef, ReactNode } from "react"
+import { Group } from "three"
+
+
+type FloatingCanProps = {
+    flavor?: SodaCanProps["flavor"];
+    floatSpeed?: number;
+    rotationIntensity?: number;
+    floatIntensity?: number;
+    floatingRange?:[number,number];
+    children?: ReactNode;
+}
+
+const FloatingCan = forwardRef<Group, FloatingCanProps>(
+    ({
+        flavor="blackCherry",
+        floatSpeed =1.5,
+        rotationIntensity=1,
+        floatIntensity=1,
+        floatingRange=[-0.1,0.1],
+        children,
+        ...props
+    }, ref)=> {
+  return (
+    <group ref={ref} {...props}>
+        <Float
+            speed={floatSpeed} //Animation speed, defaults to 1
+            rotationIntensity={rotationIntensity} //XYZ rotation intensity, defaults to 1
+            floatIntensity={floatIntensity} //Up/down float intensity, works like a multiiplyer with floatingRange, defaults to 1
+            floatingRange={floatingRange} 
+        >
+            {children}
+            <SodaCan flavor={flavor}/>
+        </Float>
+    </group>
+  )
+})
+
+FloatingCan.displayName = "FloatingCan"
+
+export default FloatingCan
--- src/components/ViewCanvas.tsx
+++ src/components/ViewCanvas.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { Canvas } from "@react-three/fiber";
 
+import { Environment, Float } from "@react-three/drei";
+import FloatingCan from "@/components/FloatingCan";
+
 type Props = {}
 
 export default function ViewCanvas({}: Props) {
@@ -22,12 +25,8 @@ export default function ViewCanvas({}: Props) {
             fov:30
         }}
         >
-        <mesh rotation={[.5,.5,0]} position={[1,0,0]}>
-            <boxGeometry />
-            <meshStandardMaterial  color={"hotpink"}/>
-        </mesh>
-        <ambientLight intensity={2} />
-        <spotLight intensity={3} position={[1,1,1]} />
+        <FloatingCan />
+        <Environment files="/hdr/lobby.hdr" environmentIntensity={1.5}/>
     </Canvas>
   )
 }
