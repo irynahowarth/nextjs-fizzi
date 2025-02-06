f8a75b28a2166f8da016bef882c8ef332b7dae41
Iryna Howarth <irynahowarth@gmail.com>
2025-02-06 13:38:52 +0000

    [feature]: add Loader to fix performance

--- src/components/ViewCanvas.tsx
+++ src/components/ViewCanvas.tsx
@@ -1,31 +1,42 @@
 "use client";
 import { Canvas } from "@react-three/fiber";
 
-import { Environment, Float, View } from "@react-three/drei";
-import FloatingCan from "@/components/FloatingCan";
+import { View } from "@react-three/drei";
+import { Suspense } from "react";
+import dynamic from "next/dynamic";
+
+const Loader = dynamic(
+  () => import("@react-three/drei").then((mod) => mod.Loader),
+  { ssr: false },
+);
 
 type Props = {}
 
 export default function ViewCanvas({}: Props) {
   return (
-    <Canvas
-        style={{
-            position: "fixed",
-            top:0,
-            left: "50%",
-            transform: "translateX(-50%)",
-            overflow: "hidden",
-            pointerEvents: "none",
-            zIndex: 30
-        }}
-        shadows
-        dpr={[1,1.5]}
-        gl={{antialias:true}}
-        camera={{
-            fov:30
-        }}
-        >
-        <View.Port />
-    </Canvas>
+    <>
+      <Canvas
+          style={{
+              position: "fixed",
+              top:0,
+              left: "50%",
+              transform: "translateX(-50%)",
+              overflow: "hidden",
+              pointerEvents: "none",
+              zIndex: 30
+          }}
+          shadows
+          dpr={[1,1.5]}
+          gl={{antialias:true}}
+          camera={{
+              fov:30
+          }}
+          >
+          <Suspense fallback={null}> 
+            <View.Port />
+          </Suspense> 
+      </Canvas>
+      <Loader />
+    </>
   )
 }
