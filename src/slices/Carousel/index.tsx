63d9f171512d3a70a0dcd8fe069e9972ae9275d2
Iryna Howarth <irynahowarth@gmail.com>
2025-02-10 13:46:48 +0000

    [feature]: add Carousel slice and basic structure. add FloatingCan

---
+++ src/slices/Carousel/index.tsx
@@ -0,0 +1,68 @@
+"use client"
+
+import FloatingCan from "@/components/FloatingCan";
+import { SodaCanProps } from "@/components/SodaCan";
+import { Content } from "@prismicio/client";
+import {PrismicText, SliceComponentProps } from "@prismicio/react";
+import { Center, Environment, View } from "@react-three/drei";
+
+
+const FLAVORS: {
+  flavor: SodaCanProps["flavor"];
+  color: string;
+  name: string;
+}[] = [
+  { flavor: "blackCherry", color: "#710523", name: "Black Cherry" },
+  { flavor: "grape", color: "#572981", name: "Grape Goodness" },
+  { flavor: "lemonLime", color: "#164405", name: "Lemon Lime" },
+  {
+    flavor: "strawberryLemonade",
+    color: "#690B3D",
+    name: "Strawberry Lemonade",
+  },
+  { flavor: "watermelon", color: "#4B7002", name: "Watermelon Crush" },
+];
+
+
+/**
+ * Props for `Carousel`.
+ */
+export type CarouselProps = SliceComponentProps<Content.CarouselSlice>;
+
+/**
+ * Component for "Carousel" Slices.
+ */
+const Carousel = ({ slice }: CarouselProps): JSX.Element => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className="carousel relative grid h-screen grid-rows-[autom 4fr, auto] justify-center overflow-hidden bg-white py-12 text-white"
+    >
+      <div className="background pointer-events-none absolute inset-0 bg-[#710523] opacity-50 " />
+      <h2 className="relative text-center text-5xl font-bold">
+        <PrismicText field={slice.primary.heading} />
+      </h2>
+      <div className="grid grid-col-[auto,auto,auto] items-center">
+        {/* Left */}
+        {/* Can */}
+        <View className="aspect-square h-[70vmin] min-h-40">
+          <Center position={[0,0,1.5]}>
+            <FloatingCan floatIntensity={.3} rotationIntensity={1} />
+          </Center>
+
+          <Environment 
+            files="/hdr/lobby.hdr" 
+            environmentIntensity={0.6}
+            environmentRotation={[0,3,0]}
+          />
+          <directionalLight intensity={6} position={[0,0,1]} />
+        </View>
+        {/* Right */}
+      </div> 
+      <PrismicText field={slice.primary.price_copy} />
+    </section>
+  );
+};
+
+export default Carousel;
